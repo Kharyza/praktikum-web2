7fa0a3527ac0a9733026a94c77b3ec961fc06b23
Kharyza <azmikhairy20@gmail.com>
2025-06-05 23:42:04 +0800

    Initial commit

---
+++ profile.php
@@ -0,0 +1,52 @@
+<div class="row">
+<div class="col-md-12">
+<div class="text-center">
+<img src="img/saya.png" alt="Dedy Rosyadi" width="160" class="rounded-circle">
+<table class="table mt-4" style="width:100%; margin:0 auto;">
+<tbody>
+<tr>
+<td align="right" width="50%"><strong>Nomor NPM :</strong></td>
+<td align="left" width="50%">2310010349</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+<td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+<td align="left" width="50%">Muhammad Azmi Khairy</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+<td align="left" width="50%">Banjarmasin, 27 Desember 2004</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>Telepon :</strong></td>
+<td align="left" width="50%">0853 4877 6212</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>email :</strong></td>
+<td align="left" width="50%">azmikhairy24@gmail.com</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>Media Sosial :</strong></td>
+<td align="left" width="50%">
+<a href="https://www.facebook.com/profile.php?id=100019593758147" target="_blank" style="text-decoration:none;">
+<img src="img/medsos/facebook.svg" width="30" alt="facebook">
+</a>
+<a href="https://www.instagram.com/azm.khry/" target="_blank" style="text-decoration:none;">
+<img src="img/medsos/instagram.svg" width="30" alt="instagram">
+</a>
+<a href="https://x.com/Kharyza1" target="_blank" style="text-decoration:none;">
+<img src="img/medsos/twitter.svg" width="30" alt="twitter">
+</a>
+<a href="https://www.linkedin.com/in/muhammad-azmi-khairy-b6039b34a/" target="_blank">
+<img src="img/medsos/linkedin.svg" width="30" alt="linkedin">
+</a>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
